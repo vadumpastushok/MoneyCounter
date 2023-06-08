45087db3e122f24304528bd7a7d632c68811334a
vadum <vadum.pastushok@gmail.com>
2023-06-08 12:20:43 +0300

    test hot fix #2

--- app/src/test/java/com/example/moneycounter/features/calculate/CalculatePresenterTest.kt
+++ app/src/test/java/com/example/moneycounter/features/calculate/CalculatePresenterTest.kt
@@ -8,8 +8,9 @@ class CalculatePresenterTest {
 
     @Test
     fun testFormatTime() {
-        val date = Date(1686213778)
+        val date = Date()
+        date.time = 1686215770000
         val locale = Locale("en")
-        assertEquals("last update 20 January 1970 year at 14:23:33", CalculatePresenter.formatTime(locale, "last update", "year", "at", date))
+        assertEquals("last update 08 June 2023 year at 12:16:10", CalculatePresenter.formatTime(locale, "last update", "year", "at", date))
     }
 }
